0312b3970332319f281a42bc84e0d9ad35d537e3
craigwang <gilgb0472>
2015-12-11 10:32:41 +0800

    search function added

--- PhotoGallery/src/main/java/com/elysium/craig/photogallery/FlickrFetchr.java
+++ PhotoGallery/src/main/java/com/elysium/craig/photogallery/FlickrFetchr.java
@@ -17,12 +17,16 @@ import java.util.ArrayList;
 
 public class FlickrFetchr {
 
-    private static final String TAG = "FlickrFetchr";
+    public static final String TAG = "FlickrFetchr";
+
+    public static final String RREF_SEARCH_QUERY = "searchQuery";
 
     private static final String ENDPOINT = "https://api.flickr.com/services/rest/";
     private static final String API_KEY = "31bdfa0de457a5b7bc8b82f82047ed2c";
     private static final String METHOD_GET_RECENT = "flickr.photos.getRecent";
-    private static final String PARAMS_EXTRA = "extras";
+    private static final String METHOD_SEARCH = "flickr.photos.search";
+    private static final String PARAM_EXTRAS = "extras";
+    private static final String PARAM_TEXT = "text";
     private static final String EXTRA_SMALL_URL = "url_s";
 
     private static final String XML_PHOTO = "photo";
@@ -56,17 +60,11 @@ public class FlickrFetchr {
         return new String(getUrlBytes(urlSpec));
     }
 
-    public static ArrayList<GalleryItem> fetchItems() {
+    public static ArrayList<GalleryItem> downloadGalleryItems(String url) {
 
         ArrayList<GalleryItem> items = new ArrayList<>();
 
         try {
-            String url = Uri.parse(ENDPOINT).buildUpon()
-                .appendQueryParameter("method", METHOD_GET_RECENT)
-                .appendQueryParameter("api_key", API_KEY)
-                .appendQueryParameter(PARAMS_EXTRA, EXTRA_SMALL_URL)
-                .build().toString();
-
             String xmlString = getUrl(url);
             Log.i(TAG, "Received xml: " + xmlString);
 
@@ -81,6 +79,27 @@ public class FlickrFetchr {
             Log.e(TAG, "Failed to parse items", e);
         }
         return items;
+    }
+
+    public static ArrayList<GalleryItem> fetchItems() {
+
+        String url = Uri.parse(ENDPOINT).buildUpon()
+            .appendQueryParameter("method", METHOD_GET_RECENT)
+            .appendQueryParameter("api_key", API_KEY)
+            .appendQueryParameter(PARAM_EXTRAS, EXTRA_SMALL_URL)
+            .build().toString();
+        return downloadGalleryItems(url);
+    }
+
+    public static ArrayList<GalleryItem> search(String query) {
+
+        String url = Uri.parse(ENDPOINT).buildUpon()
+            .appendQueryParameter("method", METHOD_SEARCH)
+            .appendQueryParameter("api_key", API_KEY)
+            .appendQueryParameter(PARAM_EXTRAS, EXTRA_SMALL_URL)
+            .appendQueryParameter(PARAM_TEXT, query)
+            .build().toString();
+        return downloadGalleryItems(url);
     }
 
     private static void parseItems(ArrayList<GalleryItem> items, XmlPullParser parser)
--- PhotoGallery/src/main/java/com/elysium/craig/photogallery/PhotoGalleryFragment.java
+++ PhotoGallery/src/main/java/com/elysium/craig/photogallery/PhotoGalleryFragment.java
@@ -1,17 +1,28 @@
 package com.elysium.craig.photogallery;
 
+import android.app.Activity;
+import android.app.SearchManager;
+import android.app.SearchableInfo;
+import android.content.ComponentName;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.SearchView;
 
 import java.util.ArrayList;
 
@@ -28,7 +39,8 @@ public class PhotoGalleryFragment extends Fragment {
 
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
-        new FetchItemsTask().execute();
+        setHasOptionsMenu(true);
+        updateItems();
 
         mThumbnailThread = new ThumbnailDownloader<>(new Handler());
         mThumbnailThread.setListener(new ThumbnailDownloader.Listener<ImageView>() {
@@ -42,6 +54,10 @@ public class PhotoGalleryFragment extends Fragment {
         mThumbnailThread.start();
         mThumbnailThread.getLooper();
         Log.i(TAG, "Background thread started");
+    }
+
+    public void updateItems() {
+        new FetchItemsTask().execute();
     }
 
     @Override
@@ -58,6 +74,47 @@ public class PhotoGalleryFragment extends Fragment {
 
         mThumbnailThread.clearQueue();
         super.onDestroyView();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_photo_gallery, menu);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+
+            MenuItem searchItem = menu.findItem(R.id.menu_item_search);
+            SearchView searchView = (SearchView) searchItem.getActionView();
+
+            SearchManager searchManager = (SearchManager) getActivity()
+                .getSystemService(Context.SEARCH_SERVICE);
+            ComponentName name = getActivity().getComponentName();
+            SearchableInfo searchInfo = searchManager.getSearchableInfo(name);
+
+            searchView.setSearchableInfo(searchInfo);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+        case R.id.menu_item_search:
+            getActivity().onSearchRequested();
+            return true;
+
+        case R.id.menu_item_clear:
+            PreferenceManager.getDefaultSharedPreferences(getActivity())
+                .edit()
+                .putString(FlickrFetchr.RREF_SEARCH_QUERY, null)
+                .commit();
+            return true;
+        
+        default:
+            return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
@@ -85,7 +142,15 @@ public class PhotoGalleryFragment extends Fragment {
 
         @Override
         protected ArrayList<GalleryItem> doInBackground(Void... voids) {
-            return FlickrFetchr.fetchItems();
+
+            Activity activity = getActivity();
+            String query = PreferenceManager.getDefaultSharedPreferences(activity)
+                .getString(FlickrFetchr.RREF_SEARCH_QUERY, null);
+            if (query != null) {
+                return FlickrFetchr.search(query);
+            } else {
+                return FlickrFetchr.fetchItems();
+            }
         }
 
         @Override
